d4e34f76bf84405493a8ee6f810d59a5b5e41965
Mattt Thompson <m@mattt.me>
2011-08-03 13:00:40 -0500

    Adding #imports for Foundation and obj-c runtime

--- AFNetworking/UIImageView+AFNetworking.m
+++ AFNetworking/UIImageView+AFNetworking.m
@@ -20,6 +20,9 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+
 #import "UIImageView+AFNetworking.h"
 
 #import "AFImageCache.h"
