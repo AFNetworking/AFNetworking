69c48f85bb251e23f1aa5e274ecf169c73e2b1a7
Mattt Thompson <m@mattt.me>
2011-09-21 15:50:42 -0500

    Removing unused -layoutSubviews code in SpotTableViewCell

--- Example/Classes/Views/SpotTableViewCell.m
+++ Example/Classes/Views/SpotTableViewCell.m
@@ -73,27 +73,4 @@
     self.detailTextLabel.text = nil;
 }
 
-#pragma mark - UIView
-
-//- (void)layoutSubviews {
-//	[super layoutSubviews];
-//	
-//	CGRect imageViewFrame = self.imageView.frame;
-//	CGRect textLabelFrame = self.textLabel.frame;
-//	CGRect detailTextLabelFrame = self.detailTextLabel.frame;
-//	
-//	imageViewFrame.origin = CGPointMake(10.0f, 10.0f);
-//	imageViewFrame.size = CGSizeMake(50.0f, 50.0f);
-//             
-//	textLabelFrame.origin.x = imageViewFrame.size.width + 25.0f;
-//    detailTextLabelFrame.origin.x = textLabelFrame.origin.x;
-//	
-//    textLabelFrame.size.width = 240.0f;
-//	detailTextLabelFrame.size.width = textLabelFrame.size.width;
-//	
-//	self.textLabel.frame = textLabelFrame;
-//	self.detailTextLabel.frame = detailTextLabelFrame;
-//	self.imageView.frame = CGRectIntegral(imageViewFrame);
-//}
-
 @end
