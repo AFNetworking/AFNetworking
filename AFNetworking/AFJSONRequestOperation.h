6832cd0e16694202a6896d3298752825f9205742
Mattt Thompson <m@mattt.me>
2011-09-21 15:34:08 -0500

    Adding #import for Foundation to AFJSONRequestOperation

--- AFNetworking/AFJSONRequestOperation.h
+++ AFNetworking/AFJSONRequestOperation.h
@@ -20,6 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#import <Foundation/Foundation.h>
 #import "AFHTTPRequestOperation.h"
 
 @interface AFJSONRequestOperation : AFHTTPRequestOperation
