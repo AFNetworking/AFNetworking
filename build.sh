97bc9212e6b92cf53060cc9f0b5303f2588d8865
Michael Nachbaur <mike@nachbaur.com>
2011-09-24 10:55:38 -0700

    Add an initial build script to generate a statically-linkable artifact

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -ex
+
+INSTALL_PATH=$WORKSPACE/artifacts
+[ -z $INSTALL_PATH ] || INSTALL_PATH=$PWD/artifacts
+
+rm -rf $INSTALL_PATH
+mkdir -p $INSTALL_PATH
+
+PROJ=AFNetworking/AFNetworking.xcodeproj 
+
+xcodebuild -project $PROJ -sdk iphoneos INSTALL_ROOT=$INSTALL_PATH/device install
+xcodebuild -project $PROJ -sdk iphonesimulator INSTALL_ROOT=$INSTALL_PATH/simulator install
+
+lipo -create -output $INSTALL_PATH/libAFNetworking.a $INSTALL_PATH/device/libAFNetworking.a $INSTALL_PATH/simulator/libAFNetworking.a
+mv $INSTALL_PATH/device/Headers $INSTALL_PATH
+rm -rf $INSTALL_PATH/device $INSTALL_PATH/simulator
