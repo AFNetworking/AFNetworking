e6a1c083a17373af3d2b96bc2392d31efd121d50
Michael Parker <michael.g.parker@gmail.com>
2011-10-07 12:08:56 -0700

    Allow sending a JSON body in a HTTP request.
    Similar to requestWithMethod:path:parameters, the added
    requestWithMethod:path:jsonBody returns a NSMutableURLRequest with the
    path resolved to a URL and headers assigned, but sets the body to the
    given JSON string value instead of creating a body from a parameter
    list.

--- AFNetworking/AFHTTPClient.h
+++ AFNetworking/AFHTTPClient.h
@@ -142,6 +142,19 @@
 ///-------------------------------
 
 /**
+ Creates a `NSMutableURLRequest` object with the specified HTTP method, path, and string containing serialized JSON for the HTTP body. Because the JSON is used as the body, the HTTP method must be `POST`, `PUT`, or `DELETE`.
+ 
+ @param method The HTTP method for the request; must be one of `POST`, `PUT`, or `DELETE`.
+ @param path The path to be appended to the HTTP client's base URL and used as the request URL.
+ @param parameters The string containing serialized JSON to be used as the request HTTP body.
+ 
+ @return An `NSMutableURLRequest` object
+ */
+- (NSMutableURLRequest *)requestWithMethod:(NSString *)method
+                                      path:(NSString *)path
+                                  jsonBody:(NSString *)jsonBody;
+
+/**
  Creates an `NSMutableURLRequest` object with the specified HTTP method and path. If the HTTP method is `GET`, the parameters will be used to construct a url-encoded query string that is appended to the request's URL. If `POST`, `PUT`, or `DELETE`, the parameters will be encoded into a `application/x-www-form-urlencoded` HTTP body.
  
  @param method The HTTP method for the request, such as `GET`, `POST`, `PUT`, or `DELETE`.
