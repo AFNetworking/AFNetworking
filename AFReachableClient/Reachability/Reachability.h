8e092de7b7789ed9d6e4252f6370eeda363596e5
Kevin Harwood <kcharwood@gmail.com>
2011-12-19 17:22:25 -0600

    Reachability fixes
    Suppressed warnings in Reachability
    Turned printing off

--- AFReachableClient/Reachability/Reachability.h
+++ AFReachableClient/Reachability/Reachability.h
@@ -48,6 +48,7 @@
 
 #import <Foundation/Foundation.h>
 #import <SystemConfiguration/SystemConfiguration.h>
+#import <netinet/in.h>
 
 typedef enum {
 	NotReachable = 0,
--- AFReachableClient/Reachability/Reachability.m
+++ AFReachableClient/Reachability/Reachability.m
@@ -56,7 +56,7 @@
 
 #import "Reachability.h"
 
-#define kShouldPrintReachabilityFlags 1
+#define kShouldPrintReachabilityFlags 0
 
 static void PrintReachabilityFlags(SCNetworkReachabilityFlags    flags, const char* comment)
 {
