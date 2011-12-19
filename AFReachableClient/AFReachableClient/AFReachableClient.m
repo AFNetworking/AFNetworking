c36f84d3725f53fbf827386a6e1a46ff7a2b968a
Kevin Harwood <kcharwood@gmail.com>
2011-12-19 16:34:06 -0600

    Added Comments

--- AFReachableClient/AFReachableClient/AFReachableClient.m
+++ AFReachableClient/AFReachableClient/AFReachableClient.m
@@ -1,10 +1,24 @@
+// AFReachableClient.h
 //
-//  AFReachableClient.m
-//  AFReachableClient
-//
-//  Created by Kevin Harwood on 12/19/11.
-//  Copyright (c) 2011 Alications. All rights reserved.
-//
+// Copyright (c) 2011 Kevin Harwood
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import "AFReachableClient.h"
 
@@ -23,6 +37,8 @@
 
 - (id)initWithBaseURL:(NSURL *)url 
      reachableHostURL:(NSURL *)reachableHostURL{
+    NSAssert(reachableHostURL != nil, @"Reachable Host URL cannont be nil");
+    
     self = [super initWithBaseURL:url];
     if (self != nil){
         reachableHostURL_ = reachableHostURL;
@@ -98,6 +114,7 @@
                                          userInfo:userInfo];
         
         [operation setHTTPError:error];
+        [operation cancel];
         operation.completionBlock();
     }
 }
