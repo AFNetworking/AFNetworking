728f5c3d75aed53ac2b58b83bc5b959850a60202
Jorge Bernal <koke@amedias.org>
2012-01-05 17:45:24 +0100

    Cancel request if there is an authentication challenge but no credentials for it Conflicts:
    	AFNetworking/AFURLConnectionOperation.m

--- AFNetworking/AFURLConnectionOperation.m
+++ AFNetworking/AFURLConnectionOperation.m
@@ -335,6 +335,8 @@ didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge
             
             if (credential) {
                 [[challenge sender] useCredential:credential forAuthenticationChallenge:challenge];
+            } else {
+                [[challenge sender] continueWithoutCredentialForAuthenticationChallenge:challenge];
             }
         } else {
             [[challenge sender] continueWithoutCredentialForAuthenticationChallenge:challenge];
