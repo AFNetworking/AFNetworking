8c2304cdfeeca71075807b64ca14cc986927369f
Mattt Thompson <m@mattt.me>
2012-01-16 14:32:44 -0800

    [Issue #158] [Issue #163] Fixing error check in appendPartWithFileURL:name:error:. Looking at the documentation for NSURLConnection +sendSynchronousRequest:returningResponse:error:, we don't really need to check error to do the right thing. Therefore, we can get away with checking data and response, and leaving the inclusion of the specified **error as an optional exercise to the user

--- AFNetworking/AFHTTPClient.m
+++ AFNetworking/AFHTTPClient.m
@@ -475,7 +475,7 @@ static inline NSString * AFMultipartFormFinalBoundary() {
     NSURLResponse *response = nil;
     NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:error];
     
-    if (response && !error) {
+    if (data && response) {
         [self appendPartWithFileData:data name:name fileName:[response suggestedFilename] mimeType:[response MIMEType]];
         
         return YES;
