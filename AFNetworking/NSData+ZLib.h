384207fbc57240cf92ca5271a4fd71067f0d6339
Menno Pruijssers <menno@roamler.com>
2012-01-24 20:58:13 +0100

    Add GZip Compression

---
+++ AFNetworking/NSData+ZLib.h
@@ -0,0 +1,13 @@
+//
+//  NSData+ZLib.h
+//  Timmas
+//
+//  Created by Menno Pruijssers on 24-01-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (ZLib)
+
+@end
---
+++ AFNetworking/NSData+ZLib.m
@@ -0,0 +1,13 @@
+//
+//  NSData+ZLib.m
+//  Timmas
+//
+//  Created by Menno Pruijssers on 24-01-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSData+ZLib.h"
+
+@implementation NSData (ZLib)
+
+@end
