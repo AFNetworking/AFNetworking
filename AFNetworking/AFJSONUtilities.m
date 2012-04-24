AFNetworking/AFNetworking

commit 7926ee1c7a755bc1710519bddde4ef0141bb9af0

Fixed bug with JSON decodes not returning errors

AFNetworking/AFJSONUtilities.m
@@ -143,7 +143,7 @@ id AFJSONDecode(NSData *data, NSError **error) {
         NSUInteger parseOptionFlags = 0;
         [invocation setArgument:&parseOptionFlags atIndex:2]; // arguments 0 and 1 are self and _cmd respectively, automatically set by NSInvocation
         if (error != NULL) {
-            [invocation setArgument:error atIndex:3];
+            [invocation setArgument:&error atIndex:3];
         }
         
         [invocation invoke];
@@ -165,7 +165,7 @@ id AFJSONDecode(NSData *data, NSError **error) {
         NSUInteger yajlParserOptions = 0;
         [invocation setArgument:&yajlParserOptions atIndex:2]; // arguments 0 and 1 are self and _cmd respectively, automatically set by NSInvocation
         if (error != NULL) {
-            [invocation setArgument:error atIndex:3];
+            [invocation setArgument:&error atIndex:3];
         }
         
         [invocation invoke];
@@ -178,7 +178,7 @@ id AFJSONDecode(NSData *data, NSError **error) {
         
         [invocation setArgument:&data atIndex:2]; // arguments 0 and 1 are self and _cmd respectively, automatically set by NSInvocation
         if (error != NULL) {
-            [invocation setArgument:error atIndex:3];
+            [invocation setArgument:&error atIndex:3];
         }
         [invocation setArgument:&nullOption atIndex:4];
         
@@ -196,7 +196,7 @@ id AFJSONDecode(NSData *data, NSError **error) {
         NSUInteger readOptions = 0;
         [invocation setArgument:&readOptions atIndex:3];
         if (error != NULL) {
-            [invocation setArgument:error atIndex:4];
+            [invocation setArgument:&error atIndex:4];
         }
 
         [invocation invoke];

commit 27dc5a3be37bff7a1ebe838886b39fa97d61f28f

Fix the double refer issue caused in commit 7926ee1c7a755bc1710519bddde4ef0141bb9af0

AFNetworking/AFJSONUtilities.m
@@ -148,7 +148,7 @@ id AFJSONDecode(NSData *data, NSError **error) {
         NSUInteger parseOptionFlags = 0;
         [invocation setArgument:&parseOptionFlags atIndex:2]; // arguments 0 and 1 are self and _cmd respectively, automatically set by NSInvocation
         if (error != NULL) {
-            [invocation setArgument:&error atIndex:3];
+            [invocation setArgument:error atIndex:3];
         }
         
         [invocation invoke];
@@ -172,7 +172,7 @@ id AFJSONDecode(NSData *data, NSError **error) {
         NSUInteger yajlParserOptions = 0;
         [invocation setArgument:&yajlParserOptions atIndex:2]; // arguments 0 and 1 are self and _cmd respectively, automatically set by NSInvocation
         if (error != NULL) {
-            [invocation setArgument:&error atIndex:3];
+            [invocation setArgument:error atIndex:3];
         }
         
         [invocation invoke];
@@ -185,7 +185,7 @@ id AFJSONDecode(NSData *data, NSError **error) {
         
         [invocation setArgument:&data atIndex:2]; // arguments 0 and 1 are self and _cmd respectively, automatically set by NSInvocation
         if (error != NULL) {
-            [invocation setArgument:&error atIndex:3];
+            [invocation setArgument:error atIndex:3];
         }
         [invocation setArgument:&nullOption atIndex:4];
         
@@ -203,7 +203,7 @@ id AFJSONDecode(NSData *data, NSError **error) {
         NSUInteger readOptions = 0;
         [invocation setArgument:&readOptions atIndex:3];
         if (error != NULL) {
-            [invocation setArgument:&error atIndex:4];
+            [invocation setArgument:error atIndex:4];
         }
 
         [invocation invoke];