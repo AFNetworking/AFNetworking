8432cb7dfd08452cdf9c437adb95d4db99c43db8
Mattt Thompson <m@mattt.me>
2012-06-11 07:57:33 -0700

    Adding additional note about not ever really having to manually increment or decrement the activity count on AFNetworkActivityIndicatorManager

--- AFNetworking/AFNetworkActivityIndicatorManager.h
+++ AFNetworking/AFNetworkActivityIndicatorManager.h
@@ -29,6 +29,8 @@
 
 /**
  `AFNetworkActivityIndicatorManager` manages the state of the network activity indicator in the status bar. When enabled, it will listen for notifications indicating that a network request operation has started or finished, and start or stop animating the indicator accordingly. The number of active requests is incremented and decremented much like a stack or a semaphore, and the activity indicator will animate so long as that number is greater than zero.
+ 
+ @discussion By setting `isNetworkActivityIndicatorVisible` to `YES` for `sharedManager`, the network activity indicator will show and hide automatically as requests start and finish. You should not ever need to call `incrementActivityCount` or `decrementActivityCount` yourself.
  */
 @interface AFNetworkActivityIndicatorManager : NSObject
 
