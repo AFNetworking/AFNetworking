2dc837c26c43826c5af2d35e0bd9cca0c9b0d0b6
Mattt Thompson <m@mattt.me>
2012-08-23 10:16:26 -0700

    Adding back -appendPartWithFileData:...

--- AFNetworking/AFHTTPClient.h
+++ AFNetworking/AFHTTPClient.h
@@ -467,16 +467,6 @@ extern NSString * AFQueryStringFromParametersWithEncoding(NSDictionary *paramete
 
 @protocol AFStreamingMultipartFormData
 
-
-/**
- Appends the HTTP headers `Content-Disposition: form-data; name=#{name}"`, followed by the encoded data and the multipart form boundary.
- 
- @param data The data to be encoded and appended to the form data.
- @param name The name to be associated with the specified data. This parameter must not be `nil`.
- */
-
-- (void)appendPartWithFormData:(NSData *)data name:(NSString *)name;
-
 /**
  Appends the HTTP header `Content-Disposition: file; filename=#{generated filename}; name=#{name}"` and `Content-Type: #{generated mimeType}`, followed by the encoded file data and the multipart form boundary.
  
@@ -490,6 +480,26 @@ extern NSString * AFQueryStringFromParametersWithEncoding(NSDictionary *paramete
  */
 
 - (BOOL)appendPartWithFileURL:(NSURL *)fileURL name:(NSString *)name error:(NSError **)error;
+
+/**
+ Appends the HTTP header `Content-Disposition: file; filename=#{filename}; name=#{name}"` and `Content-Type: #{mimeType}`, followed by the encoded file data and the multipart form boundary.
+ 
+ @param data The data to be encoded and appended to the form data.
+ @param name The name to be associated with the specified data. This parameter must not be `nil`.
+ @param mimeType The MIME type of the specified data. (For example, the MIME type for a JPEG image is image/jpeg.) For a list of valid MIME types, see http://www.iana.org/assignments/media-types/. This parameter must not be `nil`.
+ @param filename The filename to be associated with the specified data. This parameter must not be `nil`.
+ */
+- (void)appendPartWithFileData:(NSData *)data name:(NSString *)name fileName:(NSString *)fileName mimeType:(NSString *)mimeType;
+
+/**
+ Appends the HTTP headers `Content-Disposition: form-data; name=#{name}"`, followed by the encoded data and the multipart form boundary.
+ 
+ @param data The data to be encoded and appended to the form data.
+ @param name The name to be associated with the specified data. This parameter must not be `nil`.
+ */
+
+- (void)appendPartWithFormData:(NSData *)data name:(NSString *)name;
+
 
 @end
 
