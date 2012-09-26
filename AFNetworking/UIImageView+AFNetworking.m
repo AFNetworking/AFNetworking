fc16eef8b2028768bd25f68f38bf967850ced645
Mattt Thompson <m@mattt.me>
2012-09-25 21:48:25 -0700

    Disabling HTTP pipelining for image request operations

--- AFNetworking/UIImageView+AFNetworking.m
+++ AFNetworking/UIImageView+AFNetworking.m
@@ -89,7 +89,6 @@ static char kAFImageRequestOperationObjectKey;
 {
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
     [request setHTTPShouldHandleCookies:NO];
-    [request setHTTPShouldUsePipelining:YES];
     [request addValue:@"image/*" forHTTPHeaderField:@"Accept"];
     
     [self setImageWithURLRequest:request placeholderImage:placeholderImage success:nil failure:nil];
