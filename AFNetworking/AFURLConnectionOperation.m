6a0b05265915ff863f162a4c9815258d4053eb46
Steven Fisher <tewha@me.com>
2012-09-26 16:47:56 -0700

    Add error when building without ARC to a central file.

--- AFNetworking/AFURLConnectionOperation.m
+++ AFNetworking/AFURLConnectionOperation.m
@@ -25,6 +25,11 @@
 #import <UIKit/UIKit.h>
 #endif
 
+#if !__has_feature(objc_arc)
+#error AFNetworking must be built with ARC.
+// You can turn on ARC for only AFNetworking files by adding -fobjc-arc to the build phase for each of its files.
+#endif
+
 typedef enum {
     AFOperationPausedState      = -1,
     AFOperationReadyState       = 1,
