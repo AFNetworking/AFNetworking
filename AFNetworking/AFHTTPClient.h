e7c786eee73963d7a4d23a71cc5114bb3b8dbbae
Mattt Thompson <m@mattt.me>
2012-10-05 10:19:25 -0700

    Adding #warning macros when SystemConfiguration or CoreServices frameworks are missing, causing functionality to be missing or altered

--- AFNetworking/AFHTTPClient.h
+++ AFNetworking/AFHTTPClient.h
@@ -78,6 +78,12 @@ typedef enum {
     AFNetworkReachabilityStatusReachableViaWWAN = 1,
     AFNetworkReachabilityStatusReachableViaWiFi = 2,
 } AFNetworkReachabilityStatus;
+#else
+#warning "SystemConfiguration framework not found in project, or not included in precompiled header. Network reachability functionality will not be available."
+#endif
+
+#ifndef __UTTYPE__
+#warning "CoreServices framework not found in project, or not included in precompiled header. Automatic MIME type detection when uploading files in multipart requests will not be available."
 #endif
 
 typedef enum {
