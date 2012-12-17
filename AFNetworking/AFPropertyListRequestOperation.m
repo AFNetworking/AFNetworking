474a940414500fe3b24b91e5f7f09806d65418bb
Jesse Collis <jesse@jcmultimedia.com.au>
2012-12-17 22:59:43 +1100

    Fixes a handful of -Wstrict-selector-match warnings raised by Xcode 4.6DP3. As noted by @0xced, casting satisfies the compiler without changing the behaviour.

--- AFNetworking/AFPropertyListRequestOperation.m
+++ AFNetworking/AFPropertyListRequestOperation.m
@@ -47,7 +47,7 @@ static dispatch_queue_t property_list_request_operation_processing_queue() {
                                                                     success:(void (^)(NSURLRequest *request, NSHTTPURLResponse *response, id propertyList))success
                                                                     failure:(void (^)(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id propertyList))failure
 {
-    AFPropertyListRequestOperation *requestOperation = [[self alloc] initWithRequest:request];
+    AFPropertyListRequestOperation *requestOperation = [(AFPropertyListRequestOperation *)[self alloc] initWithRequest:request];
     [requestOperation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
         if (success) {
             success(operation.request, operation.response, responseObject);
