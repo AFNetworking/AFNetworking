6f5c2ffc432ec8d12a6d8992f4f39313650e7196
Raymond Kim <rkim@MBP-24292.local>
2013-01-14 16:09:40 -0800

    New static method to query whether a UIImageView has been cached in memory

--- AFNetworking/UIImageView+AFNetworking.m
+++ AFNetworking/UIImageView+AFNetworking.m
@@ -149,6 +149,14 @@ static char kAFImageRequestOperationObjectKey;
     self.af_imageRequestOperation = nil;
 }
 
++ (BOOL)isImageWithURLCached:(NSURL *)url
+{
+    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:url];
+    
+    UIImage *cachedImage = [[[self class] af_sharedImageCache] cachedImageForRequest:urlRequest];
+    return (cachedImage != nil);
+}
+
 @end
 
 #pragma mark -
