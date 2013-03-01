3c316f096bb458889f95f0f48ca0502d344307e6
Vincent Roy Chevalier <vincent.roychevalier@gmail.com>
2013-02-28 21:11:02 -0500

    Update AFFileDownloadOperation.m
    FIXES overried AFURLConnectionOperation's method connectionDidFinishLoading:

--- AFNetworking/AFFileDownloadOperation.m
+++ AFNetworking/AFFileDownloadOperation.m
@@ -2,6 +2,8 @@
 
 @implementation AFFileDownloadOperation
 
+@synthesize destinationFilePath = _destinationFilePath;
+
 - (id)initWithRequest:(NSURLRequest *)urlRequest destinationFilePath:(NSString *)filePath {
     self = [super )initWithRequest:urlRequest];
     if (!self) {
@@ -11,6 +13,16 @@
     self.outputStream = [NSOutputStream outputStreamToFileAtPath:filePath append:NO];
     
     return self;
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection __unused *)connection {
+    self.responseData = [NSData dataWithContentsOfFile:_destinationFilePath options:NSDataReadingMappedIfSafe error:nil];
+
+    [self.outputStream close];
+
+    [self finish];
+
+    self.connection = nil;
 }
 
 @end
