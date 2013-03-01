5090a0dd5feefd8869af7217f5f434e4139e6a21
Vincent Roy Chevalier <vincent.roychevalier@gmail.com>
2013-02-28 21:03:54 -0500

    Update AFFileDownloadOperation.m
    ADDS implementation for initWithRequest:destinationFilePath:

--- AFNetworking/AFFileDownloadOperation.m
+++ AFNetworking/AFFileDownloadOperation.m
@@ -1,6 +1,16 @@
-
 #import "AFFileDownloadOperation.h"
 
 @implementation AFFileDownloadOperation
+
+- (id)initWithRequest:(NSURLRequest *)urlRequest destinationFilePath:(NSString *)filePath {
+    self = [super )initWithRequest:urlRequest];
+    if (!self) {
+  	  return nil;
+    }
+
+    self.outputStream = [NSOutputStream outputStreamToFileAtPath:filePath append:NO];
+    
+    return self;
+}
 
 @end
