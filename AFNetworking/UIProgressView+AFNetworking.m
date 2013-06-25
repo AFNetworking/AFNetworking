5e59e8b088383f861b20f02a1aef57ea078e0284
Mattt Thompson <m@mattt.me>
2013-06-25 14:10:22 -0700

    Adding UIProgressView+AFNetworking

---
+++ AFNetworking/UIProgressView+AFNetworking.m
@@ -0,0 +1,118 @@
+// UIProgressView+AFNetworking.m
+//
+// Copyright (c) 2013 AFNetworking (http://afnetworking.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "UIProgressView+AFNetworking.h"
+
+#import "AFURLConnectionOperation.h"
+
+@interface AFURLConnectionOperation (_UIProgressView)
+@property (readwrite, nonatomic, copy) void (^uploadProgress)(NSUInteger bytes, long long totalBytes, long long totalBytesExpected);
+@property (readwrite, nonatomic, copy) void (^downloadProgress)(NSUInteger bytes, long long totalBytes, long long totalBytesExpected);
+@end
+
+@implementation AFURLConnectionOperation (_UIProgressView)
+@dynamic uploadProgress;
+@dynamic downloadProgress;
+@end
+
+#pragma mark -
+
+@interface UIProgressView (_AFNetworking) <NSURLSessionDownloadDelegate>
+@end
+
+@implementation UIProgressView (AFNetworking)
+
+- (void)setProgressWithUploadProgressOfTask:(NSURLSessionUploadTask *)task
+                                   animated:(BOOL)animated
+{
+    [task addObserver:self forKeyPath:@"state" options:0 context:nil];
+    [task addObserver:self forKeyPath:@"countOfBytesSent" options:0 context:nil];
+}
+
+- (void)setProgressWithDownloadProgressOfTask:(NSURLSessionDownloadTask *)task
+                                     animated:(BOOL)animated
+{
+    [task addObserver:self forKeyPath:@"state" options:0 context:nil];
+    [task addObserver:self forKeyPath:@"countOfBytesReceived" options:0 context:nil];
+}
+
+- (void)setProgressWithUploadProgressOfOperation:(AFURLConnectionOperation *)operation
+                                        animated:(BOOL)animated
+{
+    __weak __typeof(self)weakSelf = self;
+    void (^original)(NSUInteger bytes, long long totalBytes, long long totalBytesExpected) = [operation.uploadProgress copy];
+    [operation setUploadProgressBlock:^(NSUInteger bytesWritten, long long totalBytesWritten, long long totalBytesExpectedToWrite) {
+        if (original) {
+            original(bytesWritten, totalBytesWritten, totalBytesExpectedToWrite);
+        }
+
+        if (totalBytesExpectedToWrite > 0) {
+            [weakSelf setProgress:(totalBytesWritten / (totalBytesExpectedToWrite * 1.0f)) animated:animated];
+        }
+    }];
+}
+
+- (void)setProgressWithDownloadProgressOfOperation:(AFURLConnectionOperation *)operation
+                                          animated:(BOOL)animated
+{
+    __weak __typeof(self)weakSelf = self;
+    void (^original)(NSUInteger bytes, long long totalBytes, long long totalBytesExpected) = [operation.uploadProgress copy];
+    [operation setDownloadProgressBlock:^(NSUInteger bytesRead, long long totalBytesRead, long long totalBytesExpectedToRead) {
+        if (original) {
+            original(bytesRead, totalBytesRead, totalBytesExpectedToRead);
+        }
+        
+        if (totalBytesExpectedToRead > 0) {
+            [weakSelf setProgress:(totalBytesRead / (totalBytesExpectedToRead  * 1.0f)) animated:animated];
+        }
+    }];
+}
+
+#pragma mark NSKeyValueObserving
+
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary *)change
+                       context:(void *)context
+{
+    // TODO invoke supersequent implementation
+    
+    if (![object isKindOfClass:[NSURLSessionTask class]]) {
+        return;
+    }
+
+    if ([keyPath isEqualToString:@"countOfBytesSent"]) {
+        if ([object countOfBytesExpectedToSend] > 0) {
+            self.progress = [object countOfBytesSent] / ([object countOfBytesExpectedToSend] * 1.0f);
+        }
+    } else if ([keyPath isEqualToString:@"countOfBytesReceived"]) {
+        if ([object countOfBytesExpectedToReceive] > 0) {
+            self.progress = [object countOfBytesReceived] / ([object countOfBytesExpectedToReceive] * 1.0f);
+        }
+    } else if ([keyPath isEqualToString:@"state"]) {
+        if ([object state] != NSURLSessionTaskStateRunning) {
+            [object removeObserver:self];
+        }
+    }
+}
+
+@end
