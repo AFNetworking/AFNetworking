b5638a9cdf01edeae09817f8b3eb4296ecaa3473
Mattt Thompson <m@mattt.me>
2013-07-19 11:32:39 -0500

    Adding AFJSONPatchSerializer (will be extracted into extension soon)

---
+++ AFNetworking/AFJSONPatchSerializer.h
@@ -0,0 +1,95 @@
+// AFJSONPatchSerializer.h
+// 
+// Copyright (c) 2013年 __MyCompanyName__
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "AFSerialization.h"
+
+// TODO extract into separate extension
+
+typedef NS_ENUM(NSUInteger, AFJSONPatchOperationType) {
+    AFJSONAddOperationType = 1,
+    AFJSONRemoveOperationType = 2,
+    AFJSONReplaceOperationType = 3,
+    AFJSONMoveOperationType = 4,
+    AFJSONCopyOperationType = 5,
+    AFJSONTestOperationType = 6,
+};
+
+/**
+ @see http://tools.ietf.org/html/rfc6902
+ */
+@interface AFJSONPatchOperation : NSObject
+
+/**
+ 
+ */
+@property (readonly, nonatomic, assign) AFJSONPatchOperationType type;
+
+/**
+ 
+ */
+@property (readonly, nonatomic, copy) NSString *path;
+
+/**
+ 
+ */
+@property (readonly, nonatomic, copy) NSString *from;
+
+/**
+ 
+ */
+@property (readonly, nonatomic, strong) id value;
+
+/**
+ 
+ */
++ (instancetype)operationWithDictionary:(NSDictionary *)dictionary
+                                  error:(NSError * __autoreleasing *)error;
+
+///
+
++ (instancetype)addOperationWithPath:(NSString *)path
+                               value:(id)value;
+
++ (instancetype)removeOperationWithPath:(NSString *)path;
+
++ (instancetype)replaceOperationWithPath:(NSString *)path
+                                   value:(id)value;
+
++ (instancetype)moveOperationFrom:(NSString *)fromPath
+                               to:(NSString *)toPath;
+
++ (instancetype)copyOperationFrom:(NSString *)fromPath
+                               to:(NSString *)toPath;
+
++ (instancetype)testOperationWithPath:(NSString *)path
+                                value:(id)value;
+
+@end
+
+#pragma mark -
+
+/**
+ 
+ */
+@interface AFJSONPatchSerializer : AFJSONSerializer
+
+@end
