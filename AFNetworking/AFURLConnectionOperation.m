6b2515d8c0f43cc06babbc1fcbd4cc15e9ef0a53
Paul Melnikow <email@paulmelnikow.com>
2013-08-12 00:15:49 -0400

    When AFNetworking is in an embedded framework, load certificates from app bundle

--- AFNetworking/AFURLConnectionOperation.m
+++ AFNetworking/AFURLConnectionOperation.m
@@ -200,7 +200,7 @@ static BOOL AFSecKeyIsEqualToKey(SecKeyRef key1, SecKeyRef key2) {
     static NSArray *_pinnedCertificates = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+        NSBundle *bundle = [NSBundle mainBundle];
         NSArray *paths = [bundle pathsForResourcesOfType:@"cer" inDirectory:@"."];
         
         NSMutableArray *certificates = [NSMutableArray arrayWithCapacity:[paths count]];
