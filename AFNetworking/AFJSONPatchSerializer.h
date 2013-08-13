ba7f50de4dcd44671a10dfc6cf286e86d36f0be3
Mattt Thompson <m@mattt.me>
2013-08-13 15:41:38 -0700

    Adding documentation for AFJSONPatchSerializer

--- AFNetworking/AFJSONPatchSerializer.h
+++ AFNetworking/AFJSONPatchSerializer.h
@@ -22,8 +22,9 @@
 
 #import "AFSerialization.h"
 
-// TODO extract into separate extension
-
+/**
+ `AFJSONPatchOperationType` represent the possible JSON Patch operation types.
+ */
 typedef NS_ENUM(NSUInteger, AFJSONPatchOperationType) {
     AFJSONAddOperationType = 1,
     AFJSONRemoveOperationType = 2,
@@ -34,61 +35,105 @@ typedef NS_ENUM(NSUInteger, AFJSONPatchOperationType) {
 };
 
 /**
+ `AFJSONPatchOperation` objects represent component operations of a JSON Patch document. JSON Patch (RFC 6902) describes a common format to represent changes in structured data.
+ 
+ A patch is comprised by an array of operations, like in the following example:
+ 
+    [
+            { "op": "add", "path": "/a/b/c", "value": [ "foo", "bar" ] },
+            { "op": "remove", "path": "/a/b/c" },
+            { "op": "replace", "path": "/a/b/c", "value": 42 },
+            { "op": "move", "from": "/a/b/c", "path": "/a/b/d" },
+            { "op": "copy", "from": "/a/b/d", "path": "/a/b/e" },
+            { "op": "test", "path": "/a/b/c", "value": "foo" }
+    ]
+ 
+ Each operation object encodes the `type` of operation (`add`, `remove`, `replace`, `move`, `copy`, or `test` for existence) on a particular `path`, and any associated `value`. `copy` and `move` operations additionally contain a `from` value.
+
  @see http://tools.ietf.org/html/rfc6902
  */
 @interface AFJSONPatchOperation : NSObject
 
 /**
- 
+ The type of operation. Possible values correspond to the operation types defined by the JSON Patch specification (`add`, `remove`, `replace`, `move`, `copy`, or `test` for existence), and are described in "AFJSONPatchOperationType".
  */
 @property (readonly, nonatomic, assign) AFJSONPatchOperationType type;
 
 /**
- 
+ The path of the resource.
  */
 @property (readonly, nonatomic, copy) NSString *path;
 
 /**
- 
+ For `move` and `copy` operations, the originating path.
  */
 @property (readonly, nonatomic, copy) NSString *from;
 
 /**
- 
+ The value associated with the operation.
  */
 @property (readonly, nonatomic, strong) id value;
 
+///-------------------------------------
+/// @name Creating JSON Patch Operations
+///-------------------------------------
+
 /**
- 
+ Create and return an `add` operation with the specified path and value.
  */
-+ (instancetype)operationWithDictionary:(NSDictionary *)dictionary
-                                  error:(NSError * __autoreleasing *)error;
-
-///
-
 + (instancetype)addOperationWithPath:(NSString *)path
                                value:(id)value;
 
+/**
+ Create and return a `remove` operation with the specified path.
+ */
 + (instancetype)removeOperationWithPath:(NSString *)path;
 
+/**
+ Create and return a `replace` operation with the specified path and value.
+ */
 + (instancetype)replaceOperationWithPath:(NSString *)path
                                    value:(id)value;
 
+/**
+ Create and return a `move` operation, with a specified origin and destination.
+ */
 + (instancetype)moveOperationFrom:(NSString *)fromPath
                                to:(NSString *)toPath;
 
+/**
+ Create and return a `copy` operation, with a specified origin and destination.
+ */
 + (instancetype)copyOperationFrom:(NSString *)fromPath
                                to:(NSString *)toPath;
 
+/**
+ Create and return a `test` operation, with a specified path and value.
+ */
 + (instancetype)testOperationWithPath:(NSString *)path
                                 value:(id)value;
+
+///
+
+/**
+ Creates and returns an operation from a dictionary.
+
+ @param dictionary The fields defining the operation.
+ @param error The error that occurred while attempting construct a JSON Patch operation.
+
+ @return A JSON Patch operation.
+ */
++ (instancetype)operationWithDictionary:(NSDictionary *)dictionary
+                                  error:(NSError * __autoreleasing *)error;
 
 @end
 
 #pragma mark -
 
 /**
- 
+ `AFServerSentEventSerializer` is a subclass of `AFHTTPSerializer` that validates and decodes server sent event messages.
+
+ By default, `AFServerSentEventSerializer` accepts responses with a MIME type of `application/json-patch+json`.
  */
 @interface AFJSONPatchSerializer : AFJSONSerializer
 
