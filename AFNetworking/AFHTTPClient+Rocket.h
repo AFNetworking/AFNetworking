6b87fcc65050960e34896beaedaaef6a8aaf266b
Mattt Thompson <m@mattt.me>
2013-08-13 15:50:48 -0700

    Adding documentation to AFHTTPClient+Rocket

--- AFNetworking/AFHTTPClient+Rocket.h
+++ AFNetworking/AFHTTPClient+Rocket.h
@@ -26,19 +26,35 @@
 #import "AFJSONPatchSerializer.h"
 
 /**
+ This category adds methods to AFNetworking's `AFHTTPClient` class related to real-time networking with Rocket.
  
+ Rocket is a technique for building real-time functionality on top of REST web services that leverages web standards like Server-Sent Events and JSON Patch. According to REST conventions, when a client makes an HTTP `GET` request to a resource endpoint, a list of records is returned. With Rocket, a client can additionally `SUBSCRIBE` to changes for that resource by requesting an event stream at that endpoint. Anytime a record is created, updated, or destroyed, an event is sent in real-time over the event stream, encoding the changes as a JSON Patch document.
+ 
+ @see http://rocket.github.io
  */
 @interface AFHTTPClient (Rocket)
 
 /**
+ Creates and opens an event source with a `SUBSCRIBE` URL request to the specified URL string, executing a block for each received event.
  
+ @param URLString The URL string used to create the request URL.
+ @param block A block object to be executed each time an event is received from the server. This block has no return value and takes two arguments: the JSON Patch operations associated with the received server-sent event, and the error that occured while receiving the last event, if any.
+ @param error The error that occurred while attempting to open the event stream.
+ 
+ @return An event source, opened to the request created with the specified URL string.
  */
 - (AFEventSource *)SUBSCRIBE:(NSString *)URLString
                   usingBlock:(void (^)(NSArray *operations, NSError *error))block
                        error:(NSError * __autoreleasing *)error;
 
 /**
+ Creates and opens an event source with the specified URL request, executing a block for each received event.
+
+ @param request The request used to connect the event source.
+ @param block A block object to be executed each time an event is received from the server. This block has no return value and takes two arguments: the JSON Patch operations associated with the received server-sent event, and the error that occured while receiving the last event, if any.
+ @param error The error that occurred while attempting to open the event stream.
  
+ @return An event source, opened to the request created with the specified URL request.
  */
 - (AFEventSource *)openEventSourceWithRequest:(NSURLRequest *)request
                                    serializer:(AFJSONPatchSerializer *)serializer
