53538d62dcf9de0d4c6b001144e66b3fd5864d4c
Kevin Harwood <kevin.harwood@mutualmobile.com>
2013-09-10 07:57:52 -0500

    Got the new ios target building with xctest

---
+++ Tests/Tests/AFTestCase.m
@@ -0,0 +1,27 @@
+//
+//  AFTestCase.m
+//  AFNetworking Tests
+//
+//  Created by Kevin Harwood on 9/10/13.
+//  Copyright (c) 2013 AFNetworking. All rights reserved.
+//
+
+#import "AFTestCase.h"
+
+NSString * const AFNetworkingTestsBaseURLString = @"http://httpbin.org/";
+
+@implementation AFTestCase
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+@end
