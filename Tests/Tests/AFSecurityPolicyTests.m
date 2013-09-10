007751685a95c2ab278ff881a2eada333d4c728e
Kevin Harwood <kevin.harwood@mutualmobile.com>
2013-09-10 10:55:32 -0500

    Added more Tests for SecurityPolicy

--- Tests/Tests/AFSecurityPolicyTests.m
+++ Tests/Tests/AFSecurityPolicyTests.m
@@ -105,4 +105,46 @@ static SecCertificateRef AFUTHTTPBinOrgCertificate(){
     XCTAssert([policy evaluateServerTrust:AFUTADNNetServerTrust()] == NO, @"HTTPBin.org Certificate Pinning Should have failed against ADN");
 }
 
+-(void)testDefaultPolicyContainsHTTPBinOrgCertificate{
+    AFSecurityPolicy * policy = [AFSecurityPolicy defaultPolicy];
+    SecCertificateRef cert = AFUTHTTPBinOrgCertificate();
+    NSData * certData = (__bridge NSData *)(SecCertificateCopyData(cert));
+    NSInteger index = [policy.pinnedCertificates
+                       indexOfObjectPassingTest:^BOOL(NSData * data, NSUInteger idx, BOOL *stop) {
+                           return [data isEqualToData:certData];
+                       }];
+    XCTAssert(index!=NSNotFound, @"HTTPBin.org certificate not found in the default certificates");
+}
+
+-(void)testDefaultPolicyIsSetToAFSSLPinningModePublicKey{
+    AFSecurityPolicy * policy = [AFSecurityPolicy defaultPolicy];
+    XCTAssert(policy.SSLPinningMode==AFSSLPinningModePublicKey, @"HTTPBin.org default policy is not set to AFSSLPinningModePublicKey.");
+}
+
+-(void)testDefaultPolicyIsSetToNotAllowInvalidSSLCertificates{
+    AFSecurityPolicy * policy = [AFSecurityPolicy defaultPolicy];
+    XCTAssert(policy.allowInvalidCertificates==NO, @"HTTPBin.org default policy should not allow invalid ssl certificates");
+}
+
+-(void)testDebugPolicyContainsHTTPBinOrgCertificate{
+    AFSecurityPolicy * policy = [AFSecurityPolicy debugPolicy];
+    SecCertificateRef cert = AFUTHTTPBinOrgCertificate();
+    NSData * certData = (__bridge NSData *)(SecCertificateCopyData(cert));
+    NSInteger index = [policy.pinnedCertificates
+                       indexOfObjectPassingTest:^BOOL(NSData * data, NSUInteger idx, BOOL *stop) {
+                           return [data isEqualToData:certData];
+                       }];
+    XCTAssert(index!=NSNotFound, @"HTTPBin.org certificate not found in the default certificates");
+}
+
+-(void)testDebugPolicyIsSetToAFSSLPinningModePublicKey{
+    AFSecurityPolicy * policy = [AFSecurityPolicy debugPolicy];
+    XCTAssert(policy.SSLPinningMode==AFSSLPinningModeNone, @"HTTPBin.org debug policy is not set to AFSSLPinningModeNone.");
+}
+
+-(void)testDebugPolicyIsSetToAllowInvalidSSLCertificates{
+    AFSecurityPolicy * policy = [AFSecurityPolicy debugPolicy];
+    XCTAssert(policy.allowInvalidCertificates==YES, @"HTTPBin.org debug policy should allow invalid ssl certificates");
+}
+
 @end
