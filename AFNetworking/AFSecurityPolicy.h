5c9d45ada5e546c7c0e65e01e70d74ddbfba3dac
Mattt Thompson <m@mattt.me>
2013-09-26 06:03:06 -0700

    Adding documentation for AFSecurityPolicy

--- AFNetworking/AFSecurityPolicy.h
+++ AFNetworking/AFSecurityPolicy.h
@@ -23,9 +23,6 @@
 #import <Foundation/Foundation.h>
 #import <Security/Security.h>
 
-/**
- 
- */
 typedef NS_ENUM(NSUInteger, AFSSLPinningMode) {
     AFSSLPinningModeNone,
     AFSSLPinningModePublicKey,
@@ -33,42 +30,89 @@ typedef NS_ENUM(NSUInteger, AFSSLPinningMode) {
 };
 
 /**
+ `AFSecurityPolicy` evaluates server trust against pinned X.509 certificates and public keys over secure connections.
  
+ Adding pinned SSL certificates to your app helps prevent man-in-the-middle attacks and other vulnerabilities. Applications dealing with sensitive customer data or financial information are strongly encouraged to route all communication over an HTTPS connection with SSL pinning configured and enabled.
  */
 @interface AFSecurityPolicy : NSObject
 
 /**
- 
+ The criteria by which server trust should be evaluated against the pinned SSL certificates. Defaults to `AFSSLPinningModeNone`.
  */
 @property (nonatomic, assign) AFSSLPinningMode SSLPinningMode;
 
 /**
- 
+ The certificates used to evaluate server trust according to the SSL pinning mode. By default, this property is set to any (`.cer`) certificates included in the app bundle.
  */
 @property (nonatomic, strong) NSArray *pinnedCertificates;
 
 /**
- 
+ Whether or not to trust servers with an invalid or expired SSL certificates. Defaults to `NO`.
  */
 @property (nonatomic, assign) BOOL allowInvalidCertificates;
 
-///
+///-----------------------------------------
+/// @name Getting Specific Security Policies
+///-----------------------------------------
 
 /**
+ Returns the shared default security policy, which does not accept invalid certificates, and does not validate against pinned certificates or public keys.
  
+ @return The default security policy.
  */
 + (instancetype)defaultPolicy;
 
+///---------------------
+/// @name Initialization
+///---------------------
+
 /**
+ Creates and returns a security policy with the specified pinning mode.
  
+ @param pinningMode The SSL pinning mode.
+ 
+ @return A new security policy.
  */
 + (instancetype)policyWithPinningMode:(AFSSLPinningMode)pinningMode;
 
-///
+///------------------------------
+/// @name Evaluating Server Trust
+///------------------------------
 
 /**
+ Whether or not the specified server trust should be accepted, based on the security policy. 
  
+ This method should be used when responding to an authentication challenge from a server.
+ 
+ @param serverTrust The X.509 certificate trust of the server.
+ 
+ @return Whether or not to trust the server.
  */
 - (BOOL)evaluateServerTrust:(SecTrustRef)serverTrust;
 
 @end
+
+///----------------
+/// @name Constants
+///----------------
+
+/**
+ ## SSL Pinning Modes
+
+ The following constants are provided by `AFSSLPinningMode` as possible SSL pinning modes.
+
+ enum {
+ AFSSLPinningModeNone,
+ AFSSLPinningModePublicKey,
+ AFSSLPinningModeCertificate,
+ }
+
+ `AFSSLPinningModeNone`
+ Do not validate servers against pinned certificates.
+
+ `AFSSLPinningModePublicKey`
+ Validate host certificates against public keys of pinned certificates.
+
+ `AFSSLPinningModeCertificate`
+ Validate host certificates against pinned certificates.
+*/
