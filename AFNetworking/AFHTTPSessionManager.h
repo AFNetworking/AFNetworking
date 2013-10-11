6441e47937f47f9320de4db47bd8804857e08d47
Bruno Koga <koga@brunokoga.com>
2013-10-11 15:10:06 -0300

    Replaces occurrences of `AFHTTPSerializer`
    Changes some comments where either AFHTTPRequestSerializer or
    AFHTTPResponseSerializer should be user insted of AFHTTPSerializer

--- AFNetworking/AFHTTPSessionManager.h
+++ AFNetworking/AFHTTPSessionManager.h
@@ -78,7 +78,7 @@
 @property (readonly, nonatomic, strong) NSURL *baseURL;
 
 /**
- Requests created with `requestWithMethod:URLString:parameters:` & `multipartFormRequestWithMethod:URLString:parameters:constructingBodyWithBlock:` are constructed with a set of default headers using a parameter serialization specified by this property. By default, this is set to an instance of `AFHTTPSerializer`, which serializes query string parameters for `GET`, `HEAD`, and `DELETE` requests, or otherwise URL-form-encodes HTTP message bodies.
+ Requests created with `requestWithMethod:URLString:parameters:` & `multipartFormRequestWithMethod:URLString:parameters:constructingBodyWithBlock:` are constructed with a set of default headers using a parameter serialization specified by this property. By default, this is set to an instance of `AFHTTPRequestSerializer`, which serializes query string parameters for `GET`, `HEAD`, and `DELETE` requests, or otherwise URL-form-encodes HTTP message bodies.
  
  @warning `requestSerializer` must not be `nil`.
  */
