93656e4fe37741cbf1b027680f6a0d984cf5e5e7
Mattt Thompson <m@mattt.me>
2013-10-25 10:44:19 +0300

    [Issue #1526] Updating documentation for default response serializer of AFHTTPRequestOperation

--- AFNetworking/AFHTTPRequestOperation.h
+++ AFNetworking/AFHTTPRequestOperation.h
@@ -39,7 +39,7 @@
 @property (readonly, nonatomic, strong) NSHTTPURLResponse *response;
 
 /**
- Responses sent from the server in data tasks created with `dataTaskWithRequest:success:failure:` and run using the `GET` / `POST` / et al. convenience methods are automatically validated and serialized by the response serializer. By default, this property is set to a JSON serializer, which serializes data from responses with a `application/json` MIME type, and falls back to the raw data object. The serializer validates the status code to be in the `2XX` range, denoting success. If the response serializer generates an error in `-responseObjectForResponse:data:error:`, the `failure` callback of the session task or request operation will be executed; otherwise, the `success` callback will be executed.
+ Responses sent from the server in data tasks created with `dataTaskWithRequest:success:failure:` and run using the `GET` / `POST` / et al. convenience methods are automatically validated and serialized by the response serializer. By default, this property is set to an AFHTTPResoinse serializer, which uses the raw data as its response object. The serializer validates the status code to be in the `2XX` range, denoting success. If the response serializer generates an error in `-responseObjectForResponse:data:error:`, the `failure` callback of the session task or request operation will be executed; otherwise, the `success` callback will be executed.
 
  @warning `responseSerializer` must not be `nil`. Setting a response serializer will clear out any cached value 
  */
