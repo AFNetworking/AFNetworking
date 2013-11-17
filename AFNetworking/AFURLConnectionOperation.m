4c20a5ea38a50f119c9e8ac3d91fb199192a1d57
Mattt Thompson <m@mattt.me>
2013-11-17 12:13:56 -0800

    [Issue #1483] Break out of loop if output stream does not have space to write bytes

--- AFNetworking/AFURLConnectionOperation.m
+++ AFNetworking/AFURLConnectionOperation.m
@@ -611,15 +611,19 @@ didReceiveResponse:(NSURLResponse *)response
             while (totalNumberOfBytesWritten < (NSInteger)length) {
                 numberOfBytesWritten = [self.outputStream write:&dataBuffer[(NSUInteger)totalNumberOfBytesWritten] maxLength:(length - (NSUInteger)totalNumberOfBytesWritten)];
                 if (numberOfBytesWritten == -1) {
-                    [self.connection cancel];
-                    [self performSelector:@selector(connection:didFailWithError:) withObject:self.connection withObject:self.outputStream.streamError];
-                    return;
-                } else {
-                    totalNumberOfBytesWritten += numberOfBytesWritten;
+                    break;
                 }
+                
+                totalNumberOfBytesWritten += numberOfBytesWritten;
             }
 
             break;
+        }
+        
+        if (self.outputStream.streamError) {
+            [self.connection cancel];
+            [self performSelector:@selector(connection:didFailWithError:) withObject:self.connection withObject:self.outputStream.streamError];
+            return;
         }
     }
 
