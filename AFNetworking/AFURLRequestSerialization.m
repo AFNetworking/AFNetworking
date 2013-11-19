6ec98906e34eb60a741971f626c21d208ef365e0
Michael Koziarski <michael@koziarski.com>
2013-11-19 13:15:25 +1300

    Experiment with setting the HTTPBody instead of the HTTPBodyStream
    This is *not* intended for merging, merely included as an example of something
    which fixes #1398

--- AFNetworking/AFURLRequestSerialization.m
+++ AFNetworking/AFURLRequestSerialization.m
@@ -448,6 +448,8 @@ NSTimeInterval const kAFUploadStream3GSuggestedDelay = 0.2;
 - (id)initWithStringEncoding:(NSStringEncoding)encoding;
 - (void)setInitialAndFinalBoundaries;
 - (void)appendHTTPBodyPart:(AFHTTPBodyPart *)bodyPart;
+
+-(NSData *) consumeStream;
 @end
 
 #pragma mark -
@@ -613,7 +615,8 @@ NSTimeInterval const kAFUploadStream3GSuggestedDelay = 0.2;
 
     // Reset the initial and final boundaries to ensure correct Content-Length
     [self.bodyStream setInitialAndFinalBoundaries];
-    [self.request setHTTPBodyStream:self.bodyStream];
+
+    [self.request setHTTPBody:[self.bodyStream consumeStream]];
 
     [self.request setValue:[NSString stringWithFormat:@"multipart/form-data; boundary=%@", kAFMultipartFormBoundary] forHTTPHeaderField:@"Content-Type"];
     [self.request setValue:[NSString stringWithFormat:@"%llu", [self.bodyStream contentLength]] forHTTPHeaderField:@"Content-Length"];
@@ -717,6 +720,25 @@ NSTimeInterval const kAFUploadStream3GSuggestedDelay = 0.2;
 
 - (BOOL)hasBytesAvailable {
     return [self streamStatus] == NSStreamStatusOpen;
+}
+
+-(NSData *) consumeStream {
+    NSMutableData *data = [NSMutableData dataWithCapacity:self.contentLength];
+
+    uint8_t buffer[1024];
+    NSInteger bytesRead = 0;
+    [self open];
+
+    while (YES) {
+        bytesRead = [self read:buffer maxLength:1024];
+        if (bytesRead == 0) {
+            break;
+        }
+        [data appendBytes:&buffer length:bytesRead];
+    }
+
+
+    return data;
 }
 
 #pragma mark - NSStream
