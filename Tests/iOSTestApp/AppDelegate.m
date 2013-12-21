7c26dcf218661e044d59586e6750940f6fefaca6
Carson McDonald <carson@ioncannon.net>
2013-12-21 16:55:00 -0500

    Add test app to force application based testing

---
+++ Tests/iOSTestApp/AppDelegate.m
@@ -0,0 +1,14 @@
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
