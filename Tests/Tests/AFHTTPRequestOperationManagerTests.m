01ce8f0903071352b3b19f80156611fbcdb15a6e
Benjamin Coe <ben@yesware.com>
2014-02-17 12:59:10 -0800

    Created test case for AFHTTPRequestOperationManager.

---
+++ Tests/Tests/AFHTTPRequestOperationManagerTests.m
@@ -0,0 +1,36 @@
+//
+//  AFHTTPRequestOperationManagerTests.m
+//  AFNetworking Tests
+//
+//  Created by Benjamin Coe on 2/17/14.
+//  Copyright (c) 2014 AFNetworking. All rights reserved.
+//
+
+#import "AFTestCase.h"
+
+#import "AFHTTPRequestOperationManager.h"
+
+@interface AFHTTPRequestOperationManagerTests : XCTestCase
+
+@end
+
+@implementation AFHTTPRequestOperationManagerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
