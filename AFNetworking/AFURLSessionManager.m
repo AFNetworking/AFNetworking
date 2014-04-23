576c5c2ce830a969ece2fd96c7e15c1d9b75f491
Padraig Kennedy <padraig.kennedy@gmail.com>
2014-04-23 12:05:10 -0700

    Revert "Remove try/catch"
    This reverts commit c239e15ce51cd725e6645f519f0c759debc7b9fc.

--- AFNetworking/AFURLSessionManager.m
+++ AFNetworking/AFURLSessionManager.m
@@ -805,9 +805,9 @@ expectedTotalBytes:(int64_t)expectedTotalBytes
                 break;
             case NSURLSessionTaskStateCompleted:
                 // AFNetworkingTaskDidFinishNotification posted by task completion handlers
-                if (![change[NSKeyValueChangeNewKey] isEqualToNumber:change[NSKeyValueChangeOldKey]]) {
+                @try {
                     [object removeObserver:self forKeyPath:@"state" context:AFTaskStateChangedContext];
-                }
+                } @catch (NSException *exception) {}
                 break;
             default:
                 break;
