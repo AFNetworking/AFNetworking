12ef83b00e02f51ebe4a8ca0249f301947aeb87d
Mattt Thompson <m@mattt.me>
2014-05-11 15:39:41 -0400

    Changing security policy to none, while new example endpoint is considered

--- Example/Classes/AFAppDotNetAPIClient.m
+++ Example/Classes/AFAppDotNetAPIClient.m
@@ -31,7 +31,7 @@ static NSString * const AFAppDotNetAPIBaseURLString = @"https://api.app.net/";
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         _sharedClient = [[AFAppDotNetAPIClient alloc] initWithBaseURL:[NSURL URLWithString:AFAppDotNetAPIBaseURLString]];
-        _sharedClient.securityPolicy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModeCertificate];
+        _sharedClient.securityPolicy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModeNone];
     });
     
     return _sharedClient;
