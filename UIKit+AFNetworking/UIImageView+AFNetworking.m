7d287078f03c7ba247c6c238a437b8683ac0035e
Pedro Piñera Buendía <pepibumur@gmail.com>
2014-05-13 12:06:00 +0200

    Sending urlRequest although the image comes from cache
    I think it would be useful to return the urlRequest in the block. this way the user can identify the returned resource ( where it comes from ) to make extra checking if needed.

--- UIKit+AFNetworking/UIImageView+AFNetworking.m
+++ UIKit+AFNetworking/UIImageView+AFNetworking.m
@@ -128,7 +128,7 @@
     UIImage *cachedImage = [[[self class] sharedImageCache] cachedImageForRequest:urlRequest];
     if (cachedImage) {
         if (success) {
-            success(nil, nil, cachedImage);
+            success(urlRequest, nil, cachedImage);
         } else {
             self.image = cachedImage;
         }
