217c54db16fa65ff0292fa0121594d2666bc6c08
FloresRobles <jeflro@gmail.com>
2014-05-22 16:22:30 -0500

    Update UIImageView+AFNetworking.m
    to prevent the double caching,  you can check for it like this

--- UIKit+AFNetworking/UIImageView+AFNetworking.m
+++ UIKit+AFNetworking/UIImageView+AFNetworking.m
@@ -144,7 +144,6 @@
         self.af_imageRequestOperation.responseSerializer = self.imageResponseSerializer;
         [self.af_imageRequestOperation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
             __strong __typeof(weakSelf)strongSelf = weakSelf;
-            [[[strongSelf class] sharedImageCache] cacheImage:responseObject forRequest:urlRequest];
             if ([[urlRequest URL] isEqual:[strongSelf.af_imageRequestOperation.request URL]]) {
                 if (success) {
                     success(urlRequest, operation.response, responseObject);
@@ -155,6 +154,9 @@
                 if (operation == strongSelf.af_imageRequestOperation){
                         strongSelf.af_imageRequestOperation = nil;
                 }
+            }
+            if ([[[strongSelf class] sharedImageCache] cachedImageForRequest:urlRequest] == nil) {
+                [[[strongSelf class] sharedImageCache] cacheImage:responseObject forRequest:urlRequest];
             }
 
         } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
