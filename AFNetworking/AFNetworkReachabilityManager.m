545c42a5a30c9e7bdff116ad70162119a824858d
Stephan Krusche <krusche@in.tum.de>
2014-07-22 22:17:53 +0200

    fix for import as cocoa touch framework as module to a swift only project
    Signed-off-by: Stephan Krusche <krusche@in.tum.de>

--- AFNetworking/AFNetworkReachabilityManager.m
+++ AFNetworking/AFNetworkReachabilityManager.m
@@ -22,6 +22,12 @@
 
 #import "AFNetworkReachabilityManager.h"
 
+#import <netinet/in.h>
+#import <netinet6/in6.h>
+#import <arpa/inet.h>
+#import <ifaddrs.h>
+#import <netdb.h>
+
 NSString * const AFNetworkingReachabilityDidChangeNotification = @"com.alamofire.networking.reachability.change";
 NSString * const AFNetworkingReachabilityNotificationStatusItem = @"AFNetworkingReachabilityNotificationStatusItem";
 
