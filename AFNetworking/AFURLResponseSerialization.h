66c07365ad82663e28c76b6cefb15adf6cac2ddb
Yannick Heinrich <yannick.heinrich@gmail.com>
2014-07-24 16:08:29 +0200

    Retrieve data on response deserialisation error
    Add new constant
    `AFNetworkingOperationFailingURLResponseOriginalDataErrorKey` to
    retrieve the original data from the request when the response is
    considered  invalid

--- AFNetworking/AFURLResponseSerialization.h
+++ AFNetworking/AFURLResponseSerialization.h
@@ -285,10 +285,18 @@ extern NSString * const AFURLResponseSerializationErrorDomain;
  These keys may exist in the user info dictionary, in addition to those defined for NSError.
 
  - `NSString * const AFNetworkingOperationFailingURLResponseErrorKey`
+ - `NSString * const AFNetworkingOperationFailingURLResponseOriginalDataErrorKey`
 
  ### Constants
 
  `AFNetworkingOperationFailingURLResponseErrorKey`
  The corresponding value is an `NSURLResponse` containing the response of the operation associated with an error. This key is only present in the `AFURLResponseSerializationErrorDomain`.
+ 
+ `AFNetworkingOperationFailingURLResponseOriginalDataErrorKey`
+ The corresponding value is an `NSData` containing the original data of the operation associated with an error. This key is only present in the `AFURLResponseSerializationErrorDomain`.
  */
 extern NSString * const AFNetworkingOperationFailingURLResponseErrorKey;
+
+extern NSString * const AFNetworkingOperationFailingURLResponseOriginalDataErrorKey;
+
+
