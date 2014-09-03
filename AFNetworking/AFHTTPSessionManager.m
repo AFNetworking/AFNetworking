7cc59a5050316d0985c43bb4a2270d84e0c70b5a
Mattt Thompson <m@mattt.me>
2014-09-03 12:44:01 -0700

    [Issue #2240] Replacing MAX_ALLOWED macro with MIN_REQUIRED

--- AFNetworking/AFHTTPSessionManager.m
+++ AFNetworking/AFHTTPSessionManager.m
@@ -273,7 +273,7 @@
     if (!configuration) {
         NSString *configurationIdentifier = [decoder decodeObjectOfClass:[NSString class] forKey:@"identifier"];
         if (configurationIdentifier) {
-#if (defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 80000) || (defined(__MAC_OS_X_VERSION_MAX_ALLOWED) && __MAC_OS_X_VERSION_MAX_ALLOWED >= 1100)
+#if (defined(__IPHONE_OS_VERSION_MIN_REQUIRED) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 80000) || (defined(__MAC_OS_X_VERSION_MIN_REQUIRED) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1100)
             configuration = [NSURLSessionConfiguration backgroundSessionConfigurationWithIdentifier:configurationIdentifier];
 #else
             configuration = [NSURLSessionConfiguration backgroundSessionConfiguration:configurationIdentifier];
