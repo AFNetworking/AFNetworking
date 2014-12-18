b04abf00a72302b7076414cddb69cd1e3dee3dc5
Johannes Plunien <plu@pqpq.de>
2014-12-18 12:49:02 +0100

    Use different image URL

--- Tests/Tests/AFImageResponseSerializerTests.m
+++ Tests/Tests/AFImageResponseSerializerTests.m
@@ -28,7 +28,7 @@
 - (void)testImageResponseSerializer {
     NSMutableArray *images = [NSMutableArray array];
     for (int i=0; i<20; i++) {
-        NSURL *url = [NSURL URLWithString:@"http://i.ebayimg.com/00/s/NzY4WDEwMjQ=/z/OoIAAOSwAL9UkVnH/$_93.JPG"];
+        NSURL *url = [NSURL URLWithString:@"https://camo.githubusercontent.com/1560be050811ab73457e90aee62cd1cd257c7fb9/68747470733a2f2f7261772e6769746875622e636f6d2f41464e6574776f726b696e672f41464e6574776f726b696e672f6173736574732f61666e6574776f726b696e672d6c6f676f2e706e67"];
         NSURLRequest *request = [NSURLRequest requestWithURL:url];
         NSURLSessionDataTask *task = [self.session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
             UIImage *image = [self.responseSerializer responseObjectForResponse:response data:data error:nil];
