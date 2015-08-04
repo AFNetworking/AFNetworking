ae9fff240e90c1466c1d56f64ad772129d0ca3f3
Jianing93 <jnzhan93@gmail.com>
2015-08-04 22:53:28 +0800

    remove unnecessary if statement

--- AFNetworking/AFHTTPRequestOperation.m
+++ AFNetworking/AFHTTPRequestOperation.m
@@ -125,18 +125,10 @@ static dispatch_group_t http_request_operation_completion_group() {
                 }
             } else {
                 id responseObject = self.responseObject;
-                if (self.error) {
-                    if (failure) {
-                        dispatch_group_async(self.completionGroup ?: http_request_operation_completion_group(), self.completionQueue ?: dispatch_get_main_queue(), ^{
-                            failure(self, self.error);
-                        });
-                    }
-                } else {
-                    if (success) {
-                        dispatch_group_async(self.completionGroup ?: http_request_operation_completion_group(), self.completionQueue ?: dispatch_get_main_queue(), ^{
-                            success(self, responseObject);
-                        });
-                    }
+                if (success) {
+                    dispatch_group_async(self.completionGroup ?: http_request_operation_completion_group(), self.completionQueue ?: dispatch_get_main_queue(), ^{
+                        success(self, responseObject);
+                    });
                 }
             }
 
