ab5776f536cc4ec69b07ef23f3d7f5206ea7430f
David Peak <dppeak@yahoo.com>
2015-09-22 15:12:57 -0500

    Add __nullable to setSharedImageCache method
    '__nullable' needs to be added to the id attribute to allow for the passing of nil for the image cache.  This was found during testing.

--- UIKit+AFNetworking/UIImageView+AFNetworking.m
+++ UIKit+AFNetworking/UIImageView+AFNetworking.m
@@ -81,7 +81,7 @@
 #pragma clang diagnostic pop
 }
 
-+ (void)setSharedImageCache:(id <AFImageCache>)imageCache {
++ (void)setSharedImageCache:(__nullable id <AFImageCache>)imageCache {
     objc_setAssociatedObject(self, @selector(sharedImageCache), imageCache, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
