330ac3c4d45be253f592563a543c9fc867360e26
Kevin Harwood <kevin.harwood@mutualmobile.com>
2015-10-02 14:35:50 -0500

    Added carthage support

---
+++ Framework/AFNetworking.h
@@ -0,0 +1,19 @@
+//
+//  AFNetworking.h
+//  AFNetworking
+//
+//  Created by Kevin Harwood on 10/2/15.
+//  Copyright © 2015 AFNetworking. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AFNetworking.
+FOUNDATION_EXPORT double AFNetworkingVersionNumber;
+
+//! Project version string for AFNetworking.
+FOUNDATION_EXPORT const unsigned char AFNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFNetworking/PublicHeader.h>
+
+
