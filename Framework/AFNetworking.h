345a21d9e0a26c5a780589a883bb11d3b34d0d1f
Kevin Harwood <kevin.harwood@mutualmobile.com>
2015-10-12 15:32:45 -0500

    Carthage updates to the framework project

--- Framework/AFNetworking.h
+++ Framework/AFNetworking.h
@@ -29,4 +29,38 @@ FOUNDATION_EXPORT const unsigned char AFNetworkingVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <AFNetworking/PublicHeader.h>
 
+#import <Availability.h>
+#import <TargetConditionals.h>
 
+#ifndef _AFNETWORKING_
+#define _AFNETWORKING_
+
+#import <AFNetworking/AFURLRequestSerialization.h>
+#import <AFNetworking/AFURLResponseSerialization.h>
+#import <AFNetworking/AFSecurityPolicy.h>
+
+#if !TARGET_OS_WATCH
+#import <AFNetworking/AFNetworkReachabilityManager.h>
+#endif
+
+#import <AFNetworking/AFURLSessionManager.h>
+#import <AFNetworking/AFHTTPSessionManager.h>
+
+#if TARGET_OS_IOS || TARGET_OS_TV
+#import <AFNetworking/AFAutoPurgingImageCache.h>
+#import <AFNetworking/AFImageDownloader.h>
+#import <AFNetworking/UIActivityIndicatorView+AFNetworking.h>
+#import <AFNetworking/UIButton+AFNetworking.h>
+#import <AFNetworking/UIImage+AFNetworking.h>
+#import <AFNetworking/UIImageView+AFNetworking.h>
+#import <AFNetworking/UIProgressView+AFNetworking.h>
+#endif
+
+#if TARGET_OS_IOS
+#import <AFNetworking/AFNetworkActivityIndicatorManager.h>
+#import <AFNetworking/UIRefreshControl+AFNetworking.h>
+#import <AFNetworking/UIWebView+AFNetworking.h>
+#endif
+
+
+#endif /* _AFNETWORKING_ */
