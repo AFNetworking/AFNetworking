c99e1b9fad14de49d88eb185c60bccfbead6c1ad
DXSmile <312692347@qq.com>
2016-04-12 13:06:08 +0800

    为acceptableContentTypes添加两种常用类型
    为acceptableContentTypes添加两种常用类型  : @"text/plain", @"text/html"

--- AFNetworking/AFURLResponseSerialization.m
+++ AFNetworking/AFURLResponseSerialization.m
@@ -225,7 +225,7 @@ static id AFJSONObjectByRemovingKeysWithNullValues(id JSONObject, NSJSONReadingO
         return nil;
     }
 
-    self.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", nil];
+    self.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", @"text/plain", @"text/html", nil];
 
     return self;
 }
