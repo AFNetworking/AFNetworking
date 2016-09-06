b06a36808706cc30ac27075e1274d2682c04e82d
zhaoyuyu <zhaoyuyu1@126.com>
2016-09-06 17:20:48 +0800

    Fixed retainCycle caused by NSTimer
    NSTimer like block will strongly retain self,caused a retainCycle

--- UIKit+AFNetworking/AFNetworkActivityIndicatorManager.m
+++ UIKit+AFNetworking/AFNetworkActivityIndicatorManager.m
@@ -76,46 +76,6 @@ typedef void (^AFNetworkActivityActionBlock)(BOOL networkActivityIndicatorVisibl
     return [_target respondsToSelector:aSelector];
 }
 
-- (BOOL)isEqual:(id)object {
-    return [_target isEqual:object];
-}
-
-- (NSUInteger)hash {
-    return [_target hash];
-}
-
-- (Class)superclass {
-    return [_target superclass];
-}
-
-- (Class)class {
-    return [_target class];
-}
-
-- (BOOL)isKindOfClass:(Class)aClass {
-    return [_target isKindOfClass:aClass];
-}
-
-- (BOOL)isMemberOfClass:(Class)aClass {
-    return [_target isMemberOfClass:aClass];
-}
-
-- (BOOL)conformsToProtocol:(Protocol *)aProtocol {
-    return [_target conformsToProtocol:aProtocol];
-}
-
-- (BOOL)isProxy {
-    return YES;
-}
-
-- (NSString *)description {
-    return [_target description];
-}
-
-- (NSString *)debugDescription {
-    return [_target debugDescription];
-}
-
 @end
 
 @interface AFNetworkActivityIndicatorManager ()
