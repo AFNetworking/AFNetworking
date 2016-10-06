95fa90666d66d38c74d827502b70274b480cf8cc
Thomas Clement <tclementdev@free.fr>
2016-10-06 18:26:20 +0200

    Fix class name comment.

--- Tests/Tests/AFPropertyListRequestSerializerTests.m
+++ Tests/Tests/AFPropertyListRequestSerializerTests.m
@@ -1,4 +1,4 @@
-// AFPropertyListResponseSerializerTests.m
+// AFPropertyListRequestSerializerTests.m
 // Copyright (c) 2011–2016 Alamofire Software Foundation ( http://alamofire.org/ )
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
