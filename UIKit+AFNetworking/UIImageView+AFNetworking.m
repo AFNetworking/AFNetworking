2a689bae4b9070795d7d4c6dd8c15c0b90cdc06a
rbbtsn0w <hamiltonsnow@gmail.com>
2017-01-08 20:28:14 +0800

    UIImageView+AFNetworking.h support gif feature.

--- UIKit+AFNetworking/UIImageView+AFNetworking.m
+++ UIKit+AFNetworking/UIImageView+AFNetworking.m
@@ -98,6 +98,13 @@
             success(urlRequest, nil, cachedImage);
         } else {
             self.image = cachedImage;
+            if (self.animationImages.count > 1) {
+                self.animationImages = cachedImage.images;
+                self.animationDuration = cachedImage.duration;
+                [self startAnimating];
+            }else{
+                [self stopAnimating];
+            }
         }
         [self clearActiveDownloadInformation];
     } else {
@@ -118,6 +125,13 @@
                                success(request, response, responseObject);
                            } else if(responseObject) {
                                strongSelf.image = responseObject;
+                               if (self.animationImages.count > 1) {
+                                   self.animationImages = cachedImage.images;
+                                   self.animationDuration = cachedImage.duration;
+                                   [self startAnimating];
+                               }else{
+                                   [self stopAnimating];
+                               }
                            }
                            [strongSelf clearActiveDownloadInformation];
                        }
