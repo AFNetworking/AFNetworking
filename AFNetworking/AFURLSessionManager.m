b013b458c5cbd7e3d9f2a1ea7263e9de55b5122c
Jeff Kelley <SlaunchaMan@gmail.com>
2018-03-15 15:12:07 -0400

    Move a brace to make Xcode happy.
    The trailing brace within the #ifdef was confusig Xcode parsing the structure of the file.

--- AFNetworking/AFURLSessionManager.m
+++ AFNetworking/AFURLSessionManager.m
@@ -149,14 +149,16 @@ typedef void (^AFURLSessionTaskCompletionHandler)(NSURLResponse *response, id re
             [weakTask suspend];
         };
 #if __has_warning("-Wunguarded-availability-new")
-        if (@available(iOS 9, macOS 10.11, *)) {
+        if (@available(iOS 9, macOS 10.11, *))
 #else
-        if ([progress respondsToSelector:@selector(setResumingHandler:)]) {
+        if ([progress respondsToSelector:@selector(setResumingHandler:)])
 #endif
+        {
             progress.resumingHandler = ^{
                 [weakTask resume];
             };
         }
+        
         [progress addObserver:self
                    forKeyPath:NSStringFromSelector(@selector(fractionCompleted))
                       options:NSKeyValueObservingOptionNew
