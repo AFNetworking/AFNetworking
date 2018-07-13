342653f27ec3e33117f0e00eb067cb81fea8f68a
bbeversdorf <bbeversdorf@users.noreply.github.com>
2018-07-13 10:15:08 -0500

    Update AFImageDownloader.m

--- UIKit+AFNetworking/AFImageDownloader.m
+++ UIKit+AFNetworking/AFImageDownloader.m
@@ -254,7 +254,7 @@
                        completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
                            dispatch_async(self.responseQueue, ^{
                                __strong __typeof__(weakSelf) strongSelf = weakSelf;
-                               AFImageDownloaderMergedTask *mergedTask = [strongSelf safelyGetMergedTask: URLIdentifier];
+                               AFImageDownloaderMergedTask *mergedTask = [strongSelf safelyGetMergedTask:URLIdentifier];
                                if ([mergedTask.identifier isEqual:mergedTaskIdentifier]) {
                                    mergedTask = [strongSelf safelyRemoveMergedTaskWithURLIdentifier:URLIdentifier];
                                    if (error) {
