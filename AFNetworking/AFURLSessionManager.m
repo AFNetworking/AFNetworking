0d94bc9d33bbe52a066f23f1314da39933ee34f8
Jakub Kaspar <kaspikk@gmail.com>
2020-04-03 10:12:51 +0200

    Fix crash on nil serverTrust

--- AFNetworking/AFURLSessionManager.m
+++ AFNetworking/AFURLSessionManager.m
@@ -1001,7 +1001,8 @@ didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
             @throw [NSException exceptionWithName:@"Invalid Return Value" reason:@"The return value from the authentication challenge handler must be nil, an NSError, an NSURLCredential or an NSNumber." userInfo:nil];
         }
     } else {
-        evaluateServerTrust = YES;
+        // serverTrust is nil if the authentication method of the protection space is not server trust
+        evaluateServerTrust = challenge.protectionSpace.serverTrust != nil;
     }
 
     if (evaluateServerTrust) {
